295b2cb75d431342deb021c62a77ae7c74a01b7c
_lordBucket <antoniuk.orest@gmail.com>
2025-11-27 20:26:43 +0100

    Trying to still fix CORS.

--- src/main/java/com/lordbucket/langlearn/config/SecurityConfig.java
+++ src/main/java/com/lordbucket/langlearn/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -52,6 +53,7 @@ public class SecurityConfig {
             JwtAuthenticationEntryPoint unauthorizedHandler
     ) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless REST API
 
                 // Set up the custom exception handler for 401 errors
